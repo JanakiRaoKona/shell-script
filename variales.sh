03286de75a67f6d38775b410647b64c134559698
janaki rao <konajanakirao@gmail.com>
2026-01-15 21:49:23 +0530

    variables

---
+++ variales.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1: Hello Suresh"
+echo "$PERSON2: Hi $PERSON2"
+echo "$PERSON1: what are you learning"
+echo "$PERSON2: Upskilling DevOps"
