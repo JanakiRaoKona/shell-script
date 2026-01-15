80e2fdb480336f73e93db7133539be4ec99ebed3
janaki rao <konajanakirao@gmail.com>
2026-01-15 22:06:28 +0530

    arguments

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# declaring variales 
+PERSON1=Ramesh # no sapce between v name and value tween = 
+PERSON2=Suresh
+
+#refering variables
+echo "$PERSON1: Hello Suresh"
+echo "$PERSON2: Hi $PERSON2"
+echo "$PERSON1: what are you learning"
+echo "$PERSON2: Upskilling DevOps"
