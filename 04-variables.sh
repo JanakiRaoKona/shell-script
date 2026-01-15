59297d37cd0cd4d5a67161382fdd0c10f2f1de45
janaki rao <konajanakirao@gmail.com>
2026-01-15 22:25:22 +0530

    usernamepassword

--- 04-variables.sh
+++ 04-variables.sh
@@ -6,4 +6,4 @@ PERSON2=$2
 echo "$PERSON1: Hello Suresh"
 echo "$PERSON2: Hi $PERSON2"
 echo "$PERSON1: what are you learning"
-echo "$PERSON2: Upskilling DevOps"git
+echo "$PERSON2: Upskilling DevOps"
---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Enter username::"
+read USERNAME
+
+echo "Enter password::"
+read PASSWORD
+
+echo "Just i will show you username: $USERNAME, password: $PASSWORD"
