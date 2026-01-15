807786141e7b64e6ba7c38734c744eefbc8f6aec
janaki rao <konajanakirao@gmail.com>
2026-01-15 23:53:45 +0530

    datatypes

--- 07-datatypes.sh
+++ 07-datatypes.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# in shell scripts no need to worry about data types if apply string it will
+# convert to int ans should be showing 0
 
 NUM1=$1
 NUM2=$2
 
 SUM=$(($NUM1+$NUM2))
 
-echo "sum of $NUM1 and $NUM2 is $SUM"
+echo "sum of ${NUM1} and ${NUM2} is $SUM"
