881b8c41e9e187ee54dc94b58f5ca134655af93a
janaki rao <konajanakirao@gmail.com>
2026-01-16 12:19:07 +0530

    installation

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 # install mysql server
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script root user access"
+else
+    echo "Your super user"
+fi
 dnf install mysql-server -y
