3874cbd23d4100eb05005d13f110bd0ffa42be38
janaki rao <konajanakirao@gmail.com>
2026-01-16 19:55:22 +0530

    install-packages

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
 # Use all concepts till now we learning install some packages.
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
 USERID=$(id -u)
+
+R="/e/[31m"
+G="/e/[32m"
+N="/e/[0m"
+Y="/e/[33m"
+
+VALIDATE () {
+    if [ $1 -ne 0 ]
+        then 
+            echo "$2 is failure....💔"
+            exit 1
+        else 
+            echo "$2 is success...✅"
+    fi
+
+}
 
 if [ $USERID -ne 0 ]
 then 
@@ -15,5 +35,12 @@ echo "All packages: $@"
 
 for i in $@
 do
-echo "Package to install: $i"
+    echo "Package to install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ] 
+        then
+            echo "Already installed $Y skip.... $N the command"
+        else
+            echo "Need to install $Y packages.. $N"
+    fi
 done
