f818afebe7a073c2351bf36c1e8003820875ec10
janaki rao <konajanakirao@gmail.com>
2026-01-17 19:11:46 +0530

    shell-script

--- expense-project/backend.sh
+++ expense-project/backend.sh
@@ -44,6 +44,5 @@ if [ $? -ne 0 ]
         useradd expense
         VALIDATE $? "User added to nodejs"
     else
-        echo -e "User expense is already created. ${Y}SKIPPING...${N}"
-
+        echo -e "User expense is already created $Y SKIPPING... $N"
 fi
