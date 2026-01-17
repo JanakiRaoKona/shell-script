8cc9d372dc2e0ebe2c8180c26390b12de3c4c2df
janaki rao <konajanakirao@gmail.com>
2026-01-18 00:17:29 +0530

    shell-script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,7 +5,9 @@ echo "Before calling other scripts : $COURSE"
 
 echo "Process ID of current shell script: $$"
 
-./16-other-script.sh
+# ./16-other-script.sh # method one 
+
+source ./16-other-script.sh # 2 nd method
 
 echo "After calling other scripts : $COURSE"
 
