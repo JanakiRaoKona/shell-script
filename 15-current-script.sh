ddf165b293fef741d55463de76bd6eb624901ca6
janaki rao <konajanakirao@gmail.com>
2026-01-17 23:37:15 +0530

    shell-script

---
+++ 15-current-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+COURSE="DevOps from current script"
+
+echo "Before calling other scripts : $COURSE"
+
+echo "Process ID of current shell script: $$"
+
+# ./16-other-script.sh
+
+echo "After calling other scripts : $COURSE"
+
