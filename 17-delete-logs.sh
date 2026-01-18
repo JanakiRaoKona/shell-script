056f9393a1e893d9944451cf86df70be11f50174
janaki rao <konajanakirao@gmail.com>
2026-01-18 22:11:54 +0530

    shell-script

--- 17-delete-logs.sh
+++ 17-delete-logs.sh
@@ -14,3 +14,6 @@ if [ -d $SOURCE_DIRECTORY ]
         echo -e "$R Please make sure $SOURCE_DIRECTORY exists $N"
         exit 1
 fi
+
+FILES = $(find . -name "*.log" -type f -mtime +7)
+echo "Files to delete $FILES"
