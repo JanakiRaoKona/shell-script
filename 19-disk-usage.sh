adce00059ae28e4bba70cb48bdf8911292c21e16
janaki rao <konajanakirao@gmail.com>
2026-01-19 11:58:30 +0530

    disk-usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,15 +2,16 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THREASOLD=75
-
+MESSAGE=""
 while IFS= read -r line
 do
 USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
 FOLDER=$(echo $line | awk -F " " '{print $NF}')
 if [ $USAGE -ge $DISK_THREASOLD ]
 then 
-    echo "$FOLDER is greatet than $DISK_THREASOLD current usage: $USAGE"
+    MESSAGE=echo "$FOLDER is greatet than $DISK_THREASOLD current usage: $USAGE"
 fi
 
 
 done <<< $DISK_USAGE
+echo "Message: $MESSAGE"
