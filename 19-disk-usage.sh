c390b860fda18cbe1ae04cb1925403c7ad9c946f
janaki rao <konajanakirao@gmail.com>
2026-01-19 19:33:59 +0530

    shell-script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_THREASOLD=20
+DISK_THREASOLD=2
 MESSAGE=""
 while IFS= read -r line
 do
@@ -14,6 +14,5 @@ fi
 
 
 done <<< $DISK_USAGE
-# echo "Message: $MESSAGE"
-
+echo "Message: $MESSAGE"
 echo "$MESSAGE" | mail -s "Disk Usage Alert 🚨" konajanakirao1998@gmail.com
