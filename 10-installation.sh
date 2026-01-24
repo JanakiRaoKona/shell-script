0bebc23836a4b7b621465e380a85527a5108b3b3
janaki rao <konajanakirao@gmail.com>
2026-01-24 10:27:31 +0530

    shell-script

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,7 +5,8 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-echo $SCRIPTNAME
+echo "this is script name: $SCRIPTNAME"
+
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
 VALIDATE () {
